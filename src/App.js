234d19d08c85d63ef40961ad0a1cb938156a1046
Mdakhib <mohamedakhib@gmail.com>
2024-06-27 12:49:32 +0530

    fix: removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import SketchDemo from "./components/SketchDemo";
-// import MidArea from "./components/MidArea";
-// import PreviewArea from "./components/PreviewArea";
-// import Sidebar from "./components/Sidebar";
-const MidArea = React.lazy(() => import("./components/MidArea"));
-const PreviewArea = React.lazy(() => import("./components/PreviewArea"));
-const Sidebar = React.lazy(() => import("./components/Sidebar"));
+
 
 export default function App() {
   return (
